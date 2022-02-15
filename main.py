2b81a70aaf8702ec458d8dfe5a935b03025318d2
Mgalazyn <m.galazynn@gmail.com>
2022-02-15 15:03:28 +0100

    TaxCalculator

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
-from shop.order import generate_order
 from shop.order import Order
 from shop.product import Product
 from shop.order_element import OrderElement
+from shop.tax_calculator import TaxCalculator
 
 
 def run_example():
-    first_order = generate_order()
-    print(first_order)
-
     cookie = Product(product_name="Ciastko", cat_name="Jedzenie", price=4)
-    first_order.add_product_to_order(cookie, quantity=10)
-    print(first_order)
+    szpinak = Product(product_name='szpinak', cat_name='Owoce i Warzywa', price=7)
+    smth = Product(product_name='cos',cat_name='inne', price=39)
+    cookies = OrderElement(cookie, quantity=10)
+    piec_szpinaku = OrderElement(szpinak, quantity=5)
+    duzo_cos = OrderElement(smth, quantity=2)
+    
+    cookies_tax = TaxCalculator.tax_for_category(cookies)
+    szpinak_tax = TaxCalculator.tax_for_category(piec_szpinaku)
+    cos_tax = TaxCalculator.tax_for_category(duzo_cos)
+
+    print(f"Cena ciastek to: {cookies.calculate_element_price()} + {cookies_tax:.2f}")
+    print(f"Cena szpinaku to: {piec_szpinaku.calculate_element_price()} + {szpinak_tax:.2f}")
+    print(f"Cena czegos to: {duzo_cos.calculate_element_price()} + {cos_tax:.2f}")
 
 
 if __name__ == '__main__':
--- shop/order.py
+++ shop/order.py
@@ -5,11 +5,15 @@ from shop.order_element import OrderElement
 
 class Order:
 
+    MAX_ORDER_ELEMENTS = 10
+
     def __init__(self, first_name, last_name, order_elements=None):
         self.last_name = last_name
         self.first_name = first_name
         if order_elements is None:
             order_elements = []
+        if len(order_elements) > Order.MAX_ORDER_ELEMENTS:
+            order_elements = order_elements[:Order.MAX_ORDER_ELEMENTS]
         self._order_elements = order_elements
         self.total_price = self._calculate_total_price()
 
@@ -20,9 +24,12 @@ class Order:
         return total_price
 
     def add_product_to_order(self, product, quantity):
-        new_element = OrderElement(product, quantity)
-        self._order_elements.append(new_element)
-        self.total_price = self._calculate_total_price()
+        if len(self._order_elements) < Order.MAX_ORDER_ELEMENTS:
+            new_element = OrderElement(product, quantity)
+            self._order_elements.append(new_element)
+            self.total_price = self._calculate_total_price()
+        else:
+            print('Osiagniete limit pozycji w zamowieniu')
 
     def __str__(self):
         order_details = f"Zamowienie zlozone przez: {self.first_name} {self.last_name}"
@@ -33,7 +40,6 @@ class Order:
 
         result = "\n".join([order_details, value_details, product_details])
         return result
-
 
     def __eq__(self, other):
         if self.__class__ != other.__class__:
@@ -53,16 +59,16 @@ class Order:
     def __len__(self):
         return len(self._order_elements)
 
-
-def generate_order():
-    number_of_product = random.randint(1, 10)
-    order_elements = []
-    for product_number in range(number_of_product):
-        product_name = f"Produkt {product_number}"
-        cat_name = "Inne"
-        price = random.randint(1, 50)
-        product = Product(product_name, cat_name, price)
-        quantity = random.randint(1, 10)
-        order_elements.append(OrderElement(product=product, quantity=quantity))
-    order = Order(first_name="Marcin", last_name="Galazyn", order_elements=order_elements)
-    return order
+    @classmethod
+    def generate_order(cls):
+        number_of_product = random.randint(1, cls.MAX_ORDER_ELEMENTS)
+        order_elements = []
+        for product_number in range(number_of_product):
+            product_name = f"Produkt {product_number}"
+            cat_name = "Inne"
+            price = random.randint(1, 50)
+            product = Product(product_name, cat_name, price)
+            quantity = random.randint(1, 10)
+            order_elements.append(OrderElement(product=product, quantity=quantity))
+        order = Order(first_name="Marcin", last_name="Galazyn", order_elements=order_elements)
+        return order
